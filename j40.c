8d08cbb04d42fa4e2d4f97ef4e0ea5c93d2330e5
Kang Seonghoon <public+git@mearie.org>
2022-08-21 16:45:06 +0900

    Rename to J40

--- j40.c
+++ j40.c
@@ -1,11 +1,11 @@
-#define JXSML_IMPLEMENTATION
-#include "jxsml.h"
+#define J40_IMPLEMENTATION
+#include "j40.h"
 
 int main(int argc, char **argv) {
 	if (argc < 2) return 1;
 	if (argc > 2) dumppath = argv[2];
 
-	jxsml_err ret = jxsml_from_file(argv[1]);
+	j40_err ret = j40_from_file(argv[1]);
 	if (ret) {
 		fprintf(stderr, "error: %c%c%c%c\n", ret >> 24 & 0xff, ret >> 16 & 0xff, ret >> 8 & 0xff, ret & 0xff);
 		return 1;
